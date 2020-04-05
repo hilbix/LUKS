48383ca0227e62a3e78001b68329e3d21af01d7c
Valentin Hilbig <hilbix@users.noreply.github.com>
2020-04-05 16:10:30 +0200

    Improved instructions

--- unlock-root.sh
+++ unlock-root.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Getting started:
-#	root
+#	sudo -i
 #	git clone https://github.com/hilbix/LUKS
 # This checks it out under /root/LUKS/
 #
@@ -9,20 +9,24 @@
 #	KEY=/dev/disk/by-id/usb-THE-DEVICE-YOU-WANT-TO-USE
 #	# The device shall be some (small) partition on your USB-key
 #	LEN=512 # or any other key length you want
-#	UUID=UUID of your encrypted drive, usually from /etc/crypttab
+#	UUID=UUID of your encrypted drive, usually found in /etc/crypttab
 #
-# Create random key on $KEY - beware, it kills any FS if the device is not empty:
+# Create random key on $KEY - beware, it kills any FS if the device $KEY is not empty:
+#	blkid $KEY | grep TYPE && echo BETTER STOP HERE, $KEY MIGHT BE USED
 #	dd if=/dev/urandom of=$KEY bs=$LEN count=1
 #
 # Enable the Key for LUKS:
 #	cryptsetup luksAddKey /dev/disk/by-uuid/$UUID <(dd if=$KEY bs=$LEN count=1)
+# You can repeat that for other encrypted disks in /etc/crypttab
 #
 # Alter this script
 #	below set KEY=$KEY and LEN=$LEN from above
 #
-# Add this script to /etc/crypttab
+# Add this script here (/root/LUKS/unlock-root.sh) to /etc/crypttab
 #	vim /etc/crypttab
-#		root_crypt UUID=$UUID none luks,keyscript=/root/LUKS/unlock-root.sh
+#		root_crypt UUID=$$$UUID$$$ none luks,keyscript=/root/LUKS/unlock-root.sh
+# Just add the ",keyscript=/root/LUKS/unlock-root.sh" to all of the devices which should
+# be automatically unlocked.  ($$$UUID$$$ usually is the UUID from above!)
 #
 # Update your ramdisk:
 #	update-initramfs -u
@@ -30,8 +34,8 @@
 # Try the reboot with the USB device.
 # If this works, try reboot without USB device.
 #
-# If satisfied, you can remove all weak passphrases on $UUID.
-# Add a secure difficult one, write it down in case the USB key breaks, put it into your bank safe.
+# If satisfied, you can remove all weak passphrases on $UUID.  (see: cryptsetup luksRemoveKey </dev/tty)
+# Add a secure difficult one (cryptsetup luksAddKey), write it down and keep it somewhere safe.
 
 KEY=/dev/disk/by-id/usb-Generic_STORAGE_DEVICE_000000009451-0:0-part1
 LEN=512
